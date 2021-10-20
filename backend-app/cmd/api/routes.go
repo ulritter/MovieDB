4d6ad15ab7d266a7981c27aa21ced212d1d48f6f
Uwe Ritter <ulritter@gmail.com>
2021-10-20 23:51:30 +0200

    secure middleware chain

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
+
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
 
 func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
+	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
@@ -22,9 +32,13 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
-	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
 
-	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
+	router.POST("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
+
+	//router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+
+	//router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCORS(router)
 }
